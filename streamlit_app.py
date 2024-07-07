2d58984d280ba37d823a0fe2ba768e9f3c7a59b8
kjattheinsightworks <168951656+kjattheinsightworks@users.noreply.github.com>
2024-07-07 08:53:15 +0000

    Added Sample Prompts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+with st.expander("Sample prompts", expanded=True):
+    st.write(
+        """
+        - What kind of information is in this database?
+        - What percentage of orders are returned?
+        - How is inventory distributed across our regional distribution centers?
+        - Do customers typically place more than one order?
+        - Which product categories have the highest profit margins?
+    """
+    )
